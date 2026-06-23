590cc95a46067d71a0f94472db046db34c56fd03
Pauline Moraa <p.miyienda@alustudent.com>
2026-06-23 20:29:56 +0300

    fetching employee information

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+!/usr/bin/python3
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get(f"{base_url}/users/{employee_id}").json()
+    todos = requests.get(
+        f"{base_url}/todos", params={"userId": employee_id}
+    ).json()
+
+    done_tasks = [task for task in todos if task.get("completed")]
+    employee_name = user.get("name")
+    total = len(todos)
+    done = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({done}/{total}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
