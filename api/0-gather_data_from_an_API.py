4cb7f7be07f7a793c798ff4b9edc279bbe6a855c
Pauline Moraa <p.miyienda@alustudent.com>
2026-06-23 20:41:10 +0300

    fetching employee information

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,20 @@
-
 #!/usr/bin/python3
-"""Script that returns TODO list progress for a given employee ID."""
 import requests
 import sys
- 
- 
+
+
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
- 
     user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
     todos = requests.get(
         "{}/todos".format(base_url), params={"userId": employee_id}
     ).json()
- 
     employee_name = user.get("name")
     total = len(todos)
     done_tasks = [task for task in todos if task.get("completed")]
     done = len(done_tasks)
- 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, done, total))
     for task in done_tasks:
-        print("\t {}".format(task.get("title")))
+        print("	 {}".format(task.get("title")))
